aa5e23a07cb0d90537b14d55e76e71feddf8d3c4
PratikUgalmugale9 <pratikugalmugale2773@gmail.com>
2024-01-25 20:46:57 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+// src/app/page.tsx
+"use client"
+import MediaPlayer from '@/components/MediaPlayer';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <MediaPlayer />
+      </div>
+    </main>
+  );
+}
